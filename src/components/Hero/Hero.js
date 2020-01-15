af46b86b571f3cbe7b68e71c7826b3f2e59b6c2a
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-15 23:59:31 +0100

    added new components- Hero and List

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,8 +3,11 @@ import styles from './Hero.scss';
 
 const Hero = () => (
   <header className={styles.component}>
+    
     <h2 className={styles.title}>List to do</h2>
-        <img className={styles.image} src="https://www.pexels.com/video/wind-turbines-on-a-foggy-day-3222552/">
+    <img className={styles.image} 
+    src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="img1" />
+    
   </header>
 );
 
