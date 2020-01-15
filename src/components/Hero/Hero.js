e4af1f64619b9e1af143c56f8696b3dc314dc13b
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-15 20:51:18 +0100

    Init project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = () => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>List to do</h2>
+        <img className={styles.image} src="https://www.pexels.com/video/wind-turbines-on-a-foggy-day-3222552/">
+  </header>
+);
+
+
+export default Hero;
---
+++ src/components/List/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
+
+class List extends React.Component {
+  render() {
+    return (
+
+    <Hero />
+    )
+  }
+}
+
+export default List;
