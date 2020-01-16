7e32e745224a2d20ee87f6c5d8d46455e5002fca
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-16 20:33:35 +0100

    added PropTypes for Hero and List

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
 const Hero = props => (
   <header className={styles.component}>
@@ -7,9 +8,12 @@ const Hero = props => (
     <h2 className={styles.title}>{props.titleText}</h2>
     <img className={styles.image}
     src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" alt="img1" />
-    
+
   </header>
 );
 
+Hero.propTypes = {
+  titleText: PropTypes.node,
+};
 
 export default Hero;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    children: PropTypes.node,
+  }
   render() {
     return (
 
