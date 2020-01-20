fc5f119ad3f215353125349d90bfa8037bd1ae3a
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-21 00:14:51 +0100

    added Creator and Button file

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,11 +5,14 @@ import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator.js'
 
 class List extends React.Component {
+  state = {
+  columns: this.props.columns || [],
+}
   static propTypes = {
     title: PropTypes.node.isRequired,
-    children: PropTypes.node,
     image: PropTypes.string,
     description: settings.defaultListDescription,
     columns: PropTypes.array,
@@ -26,9 +29,12 @@ class List extends React.Component {
             <h3>{this.props.description}</h3>
         </div>
         <div className={styles.columns}>
-          {this.props.columns.map(item => (
-            <Column key={item.title} titleColumn={item.title}/>)
-          )}
+        {this.state.columns.map(({key, ...columnProps}) => (
+          <Column key={key} {...columnProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
