920225a8586d8fa56ff7fd725b21791176bd9554
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-01-29 14:29:13 +0100

    Add Search Component to App.js-show in browser

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/ListContainer.js';
+import Search  from '../Search/SearchContainer.js';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
   static propTypes = {
     title: PropTypes.node,
     subtitle: PropTypes.node,
+
     lists: PropTypes.array,
   }
   render() {
@@ -15,6 +17,8 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
+
+        {<Search/>}
 
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
