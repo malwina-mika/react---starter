8522a2d4004fa1ff034eef45d768247c3fc153fd
Malwina8 <48411653+Malwina8@users.noreply.github.com>
2020-02-01 00:05:25 +0100

    Add all functionalities to ListLink

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import FAQ from '../FAQ/FAQ';
 import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import {AnimatedSwitch} from 'react-router-transition';
+import List from '../List/ListContainer';
 
 const App = () => (
   <div>
@@ -20,6 +21,7 @@ const App = () => (
           <Route exact path='/' component={Home} />
           <Route exact path='/info' component={Info} />
           <Route exact path='/FAQ' component={FAQ} />
+          <Route exact path="/list/:id" component={List} />
         </AnimatedSwitch >
       </MainLayout>
     </BrowserRouter>
